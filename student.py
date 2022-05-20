9aea0a270d77aca1d0b14e8c7cb2850027b733f6
nasremp <63264076+nasremp@users.noreply.github.com>
2022-05-20 03:13:31 +0200

    Update student.py

--- student.py
+++ student.py
@@ -2,9 +2,72 @@
 # Written by Eleanor Tursman, based on previous work by Henry Hu,
 # Grady Williams, and James Hays for CSCI 1430 @ Brown and
 # CS 4495/6476 @ Georgia Tech
-
+import cv2
+import os
+import glob
 import numpy as np
 
+def CameraCalibrate(x):
+# Defining the dimensions of checkerboard
+    CHECKERBOARD = (7,7)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+     
+    # Creating vector to store vectors of 3D points for each checkerboard image
+    objpoints = []
+    # Creating vector to store vectors of 2D points for each checkerboard image
+    imgpoints = [] 
+
+
+    # Defining the world coordinates for 3D points
+    objp = np.zeros((1, CHECKERBOARD[0]*CHECKERBOARD[1], 3), np.float32)
+    objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+    prev_img_shape = None
+
+    # Extracting path of individual image stored in a given directory
+    images = glob.glob(x)
+    for fname in images:
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        # If desired number of corners are found in the image then ret = true
+        ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+
+    # If desired number of corner are detected, we refine the pixel coordinates and display them on the images of checker board
+        print(ret)
+        if ret == True:
+            objpoints.append(objp)
+            # refining pixel coordinates for given 2d points.
+            corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        
+            imgpoints.append(corners2)
+            
+            # Draw and display the corners
+            img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2,ret)
+    
+        cv2.imshow('img',img)
+        cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
+
+    h,w = img.shape[:2]
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+
+    # Performing camera calibration by passing the value of known 3D points (objpoints) and corresponding pixel coordinates of the detected corners (imgpoints)
+
+    print("Camera matrix : \n")
+    print(mtx)
+    print("dist : \n")
+    print(dist)
+    print("rvecs : \n")
+    print(rvecs)
+    print("tvecs : \n")
+    print(tvecs)
+
+    R = cv2.Rodrigues(rvecs[0])[0]
+    t = tvecs[0]
+    Rt = np.concatenate([R,t], axis=-1) # [R|t]
+    P = np.matmul(mtx,Rt) # A[R|t]
+    return P,objpoints,imgpoints,mtx,gray,dist
 
 # Returns the projection matrix for a given set of corresponding 2D and
 # 3D points. 
@@ -40,6 +103,7 @@ def calculate_projection_matrix(Points_2D, Points_3D):
     matrixA = []
     matrixB = []
     rows, cols = Points_2D.shape
+
     # This M matrix came from a call to rand(3,4). It leads to a high residual.
     # Your total residual should be less than 1.
     #print('Randomly setting matrix entries as a placeholder')
